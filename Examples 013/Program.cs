32420a8ba7fa800493fee72205585261c835d6ad
Ilya <happy_il@MacBook-Pro-ILYA.local>
2022-11-29 21:10:21 +0300

    New examples lesson3

---
+++ Examples 013/Program.cs
@@ -0,0 +1,13 @@
+﻿string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    for(int i = 0; i < count; i++);
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res =  Method4(10, "qwerty text");
+Console.WriteLine(res);
